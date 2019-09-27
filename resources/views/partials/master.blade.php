83cd06e293caba17cd9a56fdefb73d934b977a15
radianceobi <radiancegeorge@gmail.com>
2019-09-27 16:24:52 +0100

    Update master.blade.php

--- resources/views/partials/master.blade.php
+++ resources/views/partials/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Fashion - eCommerce HTML5 Template</title>
+    <title>Aba Global</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
